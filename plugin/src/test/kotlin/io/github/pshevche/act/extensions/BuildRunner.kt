a697aeb1af5c7569974d0b3141eb49eac330409b
pshevche <pavel.shevchenko.95@gmail.com>
2024-03-22 10:39:38 +0100

    Simplify test structure

---
+++ plugin/src/test/kotlin/io/github/pshevche/act/extensions/BuildRunner.kt
@@ -0,0 +1,15 @@
+package io.github.pshevche.act.extensions
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+
+class BuildRunner(private val workspace: Workspace) {
+
+    fun build(vararg args: String): BuildResult =
+        GradleRunner.create()
+            .forwardOutput()
+            .withPluginClasspath()
+            .withArguments(args.toList())
+            .withProjectDir(workspace.dir)
+            .build()
+}
