7a89420ad08c13560d1c714068de4c19a1f25aff
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2024-03-24 20:52:27 +0100

    Suppress unused warnings for plugin class

--- plugin/src/main/kotlin/io/github/pshevche/act/ActPlugin.kt
+++ plugin/src/main/kotlin/io/github/pshevche/act/ActPlugin.kt
@@ -6,6 +6,7 @@ import org.gradle.api.Project
 /**
  * Gradle plugin for running and validating GitHub actions using [nektos/act](https://github.com/nektos/act).
  */
+@Suppress("unused")
 class ActPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         // do nothing, as the plugin only exposes custom task types
