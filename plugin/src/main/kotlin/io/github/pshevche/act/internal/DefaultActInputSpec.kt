e3457b10777a111bf77129857411e38b30115ebb
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2024-04-07 21:34:41 +0200

    Rename the act input type

--- plugin/src/main/kotlin/io/github/pshevche/act/internal/DefaultActInputSpec.kt
+++ plugin/src/main/kotlin/io/github/pshevche/act/internal/DefaultActInputSpec.kt
@@ -1,12 +1,12 @@
 package io.github.pshevche.act.internal
 
-import io.github.pshevche.act.GithubActionInput
+import io.github.pshevche.act.ActInputSpec
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
 import java.io.File
 
-class DefaultGithubActionInput(objects: ObjectFactory, fileConvention: File) : GithubActionInput {
+class DefaultActInputSpec(objects: ObjectFactory, fileConvention: File) : ActInputSpec {
     override val file: Property<File> = objects.property(File::class.java).convention(fileConvention)
     override val values: MapProperty<String, String> =
         objects.mapProperty(String::class.java, String::class.java).convention(emptyMap())
