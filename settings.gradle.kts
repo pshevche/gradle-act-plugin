82dd8e7534ac90c0e17dfe3ee680b20d2fcf6ced
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2024-07-11 21:25:17 +0200

    Apply Develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
+plugins {
+    id("com.gradle.develocity") version "3.17.5"
+}
+
 rootProject.name = "gradle-act-plugin"
 
 include("plugin")
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+        termsOfUseAgree = "yes"
+        publishing.onlyIf {
+            false
+        }
+    }
+}
