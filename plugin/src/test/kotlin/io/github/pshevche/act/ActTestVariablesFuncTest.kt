479ce0ddb6f5a68294be7cf6e98e68a939deedda
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2024-07-24 22:40:03 +0200

    Add tests for defining workflow variables

---
+++ plugin/src/test/kotlin/io/github/pshevche/act/ActTestVariablesFuncTest.kt
@@ -0,0 +1,60 @@
+package io.github.pshevche.act
+
+import io.github.pshevche.act.fixtures.GradleProject
+import io.github.pshevche.act.fixtures.assertEvents
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.engine.spec.tempdir
+import io.kotest.matchers.string.shouldContain
+import org.opentest4j.reporting.events.core.Result.Status.SUCCESSFUL
+
+class ActTestVariablesFuncTest : FreeSpec({
+
+    val project = extension(GradleProject(tempdir()))
+
+    beforeTest {
+        project.addWorkflow("print_variables")
+    }
+
+    "supports setting variables values directly" {
+        project.addSpec(
+            "variables_values.act.yml", """
+            name: variables values
+            workflow: print_variables.yml
+            variables:
+                values:
+                    GREETING: Hello
+                    NAME: Bruce
+        """.trimIndent()
+        )
+
+        val result = project.test("-Dact.forwardOutput=true")
+
+        result.output shouldContain "Hello, Bruce!"
+        assertEvents(project.xmlReport()) {
+            spec(name = "variables values", result = SUCCESSFUL) {
+                job(name = "print_greeting", result = SUCCESSFUL)
+            }
+        }
+    }
+
+    "supports setting variables from file" {
+        project.addSpecResource("inputs/greeting.variables")
+        project.addSpec(
+            "variables_files.act.yml", """
+            name: variables file
+            workflow: print_variables.yml
+            variables:
+                file: inputs/greeting.variables
+        """.trimIndent()
+        )
+
+        val result = project.test("-Dact.forwardOutput=true")
+
+        result.output shouldContain "Hallo, Falco!"
+        assertEvents(project.xmlReport()) {
+            spec(name = "variables file", result = SUCCESSFUL) {
+                job(name = "print_greeting", result = SUCCESSFUL)
+            }
+        }
+    }
+})
